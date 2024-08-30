da970bf9556ebd997e8ab7de82590c52b9e1466f
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-08-30 10:12:58 +0200

    fix: json for deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luca/travel-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7a81e93df66788ab2f49b9b72cb87ec556af1fe4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luca/travel-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7a81e93df66788ab2f49b9b72cb87ec556af1fe4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
